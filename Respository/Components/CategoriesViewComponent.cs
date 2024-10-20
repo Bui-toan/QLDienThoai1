1649cbf20adeb89f97a8a70fce185d1e0811de7d
buitoan123 <khanhtoanx19@gmail.com>
2024-10-20 13:24:34 +0700

    Components

---
+++ Respository/Components/CategoriesViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using QLDienThoai.Models;
+
+namespace QLDienThoai.Respository.Components
+{
+	public class CategoriesViewComponent : ViewComponent
+	{
+		private readonly QldienThoaiContext _dataContext;
+		public CategoriesViewComponent(QldienThoaiContext dataContext)
+		{
+			_dataContext = dataContext;
+		}
+		public async Task<IViewComponentResult> InvokeAsync() => View(_dataContext.Categories.ToListAsync());
+	}
+}
