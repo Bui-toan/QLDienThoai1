0fb0a0f251bf224044c2c96e34150940c6eb1ac9
buitoan123 <khanhtoanx19@gmail.com>
2024-10-21 11:51:54 +0700

    Brand

---
+++ Controllers/BrandController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using QLDienThoai.Models;
+namespace QLDienThoai.Controllers
+{
+	public class BrandController : Controller
+	{
+		private readonly QldienThoaiContext _dataContext;
+		public BrandController(QldienThoaiContext dataContext)
+		{
+			_dataContext = dataContext;
+		}
+		public async Task<IActionResult> Index(String Slug = "")
+		{
+			Brand brand = _dataContext.Brands.Where(c => c.Slug == Slug).FirstOrDefault();
+			if (brand == null)
+			{
+				return RedirectToAction("Index");
+			}
+			var productsbyBrand = _dataContext.SanPhams.Where(p => p.BrandId == brand.BrandId);
+			return View(await productsbyBrand.OrderByDescending(p => p.CategoriesId).ToListAsync());
+		}
+	}
+}
