59ee1791b18ed5221a78ffc485086d63564c18f9
buitoan123 <khanhtoanx19@gmail.com>
2024-10-27 09:58:24 +0700

    Product Detail

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,16 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using QLDienThoai.Models;
 
 namespace QLDienThoai.Controllers
 {
-	public class ProductController : Controller
-	{
-		public IActionResult Index()
-		{
-			return View();
-		}
-		public IActionResult Details()
-		{
-			return View();
-		}
-	}
+    public class ProductController : Controller
+    {
+        public readonly QldienThoaiContext _dataContext;
+        public ProductController(QldienThoaiContext context)
+        {
+            _dataContext = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Details(int IdSanPham)
+        {
+            if (IdSanPham < 0)
+            {
+                return RedirectToAction("Index");
+            }
+            var productsById = _dataContext.SanPhams.Where(p => p.IdSanPham == IdSanPham).FirstOrDefault();
+
+            return View(productsById);
+        }
+    }
 }
