3950561a584d3aa40a1a0db61640fd74f2010717
Phudayy123 <fcgiangle10@gmail.com>
2024-11-05 19:03:29 +0700

    Admin Contact Edit

--- Models/ContactModel.cs
+++ Models/ContactModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace QLDienThoai.Models
@@ -8,18 +9,35 @@ namespace QLDienThoai.Models
         
         public string? Name { get; set; }
 
-        public string Map {  get; set; }
+        public string? Map {  get; set; }
 
-        public string Phone { get; set; }
+        public string? Phone { get; set; }
 
-        public string Email { get; set; }
+        public string? Email { get; set; }
 
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
-        public string LogoImg { get; set; }
+        
+        public string? LogoImg { get; set; }
 
         [NotMapped]
-        public IFormFile ImageUpLoad { get; set; }
-     
+        public IFormFile? ImageUpLoad { get; set; }
+
+        public static implicit operator ContactModel?(Contact? v)
+        {
+            if (v == null) return null;
+
+            return new ContactModel
+            {
+                Name = v.Name,
+                Map = v.Map,
+                Phone = v.Phone,
+                Email = v.Email,
+                Description = v.Description,
+                LogoImg = v.LogoImg
+                // Giả sử các thuộc tính này có mặt trong lớp Contact
+            };
+        }
+
     }
 }
