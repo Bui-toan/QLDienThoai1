4826ce61e4443c8f1f6ec13faa84e8308763278d
buitoan123 <khanhtoanx19@gmail.com>
2024-11-18 16:47:51 +0700

    Update detail

---
+++ Models/ViewModel/ProductDetailsViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace QLDienThoai.Models.ViewModel
+{
+	public class ProductDetailsViewModel
+	{
+		public SanPham ProductDetails { get; set; }
+		public DanhGia RatingDetails { get; set; }
+	}
+}
