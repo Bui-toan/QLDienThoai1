8bffa4a729eeda68722170249a75d205cf7fb17e
TunGTun <tung141004@gmail.com>
2024-11-19 18:21:12 +0700

    Fix Create Account

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -9,7 +9,7 @@ namespace QLDienThoai.Models
 		[Required(ErrorMessage = "Vui lòng nhập UserName!")]
 		public string? UserName { get; set; }
 
-		[Required(ErrorMessage = "Vui lòng nhập họ tên!")]
+		//[Required(ErrorMessage = "Vui lòng nhập họ tên!")]
 		public string? FullName { get; set; }
 
 		[Required(ErrorMessage = "Vui lòng nhập Email!"), EmailAddress]
@@ -18,7 +18,7 @@ namespace QLDienThoai.Models
 		[Required(ErrorMessage = "Vui lòng nhập số điện thoại!"), Phone]
 		public string? PhoneNumber { get; set; }
 
-		[Required(ErrorMessage = "Vui lòng nhập địa chỉ!")]
+		//[Required(ErrorMessage = "Vui lòng nhập địa chỉ!")]
 		public string? Address { get; set; }
 
 		[DataType(DataType.Password), Required(ErrorMessage = "Vui lòng nhập Password!")]
